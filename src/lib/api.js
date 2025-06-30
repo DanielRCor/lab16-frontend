b1209f354b730d7040fa8a3e308e4e14a1484d15
DanielRCor <thedaniel0405@gmail.com>
2025-06-30 00:47:10 -0500

    aaaa

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://lab16-backend-production.up.railway.app/';
+const BASE_URL = 'https://lab16-backend-production.up.railway.app';
 
 // PRODUCTOS
 export async function getProductos() {
